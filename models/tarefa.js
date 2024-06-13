ec2d59c6853946459bfa6a54a4d1b222143ba898
nicolasfernandosap <nicolasfernandosap@gmail.com>
2024-06-12 21:09:41 -0300

    adicionando

---
+++ models/tarefa.js
@@ -0,0 +1,9 @@
+class Tarefa { 
+    constructor(id, title, description) { 
+    this.id = id; 
+    this.title = title; 
+    this.description = description; 
+    } 
+    } 
+    module.exports = Tarefa;
+    
